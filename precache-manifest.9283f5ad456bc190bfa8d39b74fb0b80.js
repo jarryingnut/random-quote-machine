876332dfcaf6bcc4e8ccc10c33b3ce9ea035eced
Abhishek Ranjan <rabhishekranjan@gmail.com>
2020-09-17 17:40:38 +0530

    Updates

---
+++ precache-manifest.9283f5ad456bc190bfa8d39b74fb0b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68849a6c4f6d8a141e1da9cb553a0a5d",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e167f820ba28a18333a2",
+    "url": "/random-quote-machine/static/js/2.7b54a662.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.7b54a662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b6e5395a3d92eff006",
+    "url": "/random-quote-machine/static/js/main.50d2bb16.chunk.js"
+  },
+  {
+    "revision": "5a57d55656c6206ad7fd",
+    "url": "/random-quote-machine/static/js/runtime-main.6db6dd27.js"
+  }
+]);
